c94aa698731006688aee73b0dbf083735c48d21a
jamesmcarthur-3999 <jamesmcarthur3999@gmail.com>
2025-03-20 14:03:32 -0700

    Fix Portfolio component to use string concatenation instead of template literals

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,29 +9,28 @@ var Portfolio = {
       this.$emit('sell-stock', stock);
     }
   },
-  template: `
-    <div class="portfolio">
-      <h2>Portfolio</h2>
-      <div class="balance">Cash: ${{ player.cash.toLocaleString() }}</div>
-      <div class="net-worth">Net Worth: ${{ netWorth.toLocaleString() }}</div>
+  template: 
+    '<div class="portfolio">' +
+      '<h2>Portfolio</h2>' +
+      '<div class="balance">Cash: ${{ player.cash.toLocaleString() }}</div>' +
+      '<div class="net-worth">Net Worth: ${{ netWorth.toLocaleString() }}</div>' +
       
-      <h3>Your Stocks:</h3>
-      <ul class="stock-list">
-        <li v-for="stock in stocks" :key="stock.id" class="stock-item">
-          <div class="stock-info">
-            <span class="stock-name">{{ stock.symbol }}</span>
-            <span class="stock-price">\${{ stock.price.toFixed(2) }}</span>
-            <span class="stock-change" :class="stock.change >= 0 ? 'positive' : 'negative'">
-              {{ stock.change >= 0 ? '+' : '' }}{{ stock.change.toFixed(2) }}%
-            </span>
-          </div>
-          <div class="stock-owned" v-if="stock.owned > 0">Owned: {{ stock.owned }}</div>
-          <div class="stock-actions">
-            <button class="btn btn-buy" @click="buyStock(stock)" :disabled="player.cash < stock.price">Buy</button>
-            <button class="btn btn-sell" @click="sellStock(stock)" :disabled="stock.owned <= 0">Sell</button>
-          </div>
-        </li>
-      </ul>
-    </div>
-  `
+      '<h3>Your Stocks:</h3>' +
+      '<ul class="stock-list">' +
+        '<li v-for="stock in stocks" :key="stock.id" class="stock-item">' +
+          '<div class="stock-info">' +
+            '<span class="stock-name">{{ stock.symbol }}</span>' +
+            '<span class="stock-price">${{ stock.price.toFixed(2) }}</span>' +
+            '<span class="stock-change" :class="stock.change >= 0 ? \'positive\' : \'negative\'">' +
+              '{{ stock.change >= 0 ? "+" : "" }}{{ stock.change.toFixed(2) }}%' +
+            '</span>' +
+          '</div>' +
+          '<div class="stock-owned" v-if="stock.owned > 0">Owned: {{ stock.owned }}</div>' +
+          '<div class="stock-actions">' +
+            '<button class="btn btn-buy" @click="buyStock(stock)" :disabled="player.cash < stock.price">Buy</button>' +
+            '<button class="btn btn-sell" @click="sellStock(stock)" :disabled="stock.owned <= 0">Sell</button>' +
+          '</div>' +
+        '</li>' +
+      '</ul>' +
+    '</div>'
 };
