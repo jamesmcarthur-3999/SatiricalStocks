64021d5d2bdb36c819678a1db9e2805ef63343a7
jamesmcarthur-3999 <jamesmcarthur3999@gmail.com>
2025-03-20 13:38:45 -0700

    Add Upgrades component

---
+++ src/components/Upgrades.js
@@ -0,0 +1,36 @@
+// Upgrades component
+const Upgrades = {
+  props: {
+    upgrades: Array,
+    playerCash: Number
+  },
+  methods: {
+    buyUpgrade(upgrade) {
+      this.$emit('buy-upgrade', upgrade);
+    }
+  },
+  template: `
+    <div class="upgrade-shop">
+      <h2 class="upgrade-title">Ways to "Optimize" Your Wealth</h2>
+      <ul class="upgrade-list">
+        <li v-for="upgrade in upgrades" :key="upgrade.id" class="upgrade-item">
+          <div class="upgrade-info">
+            <div class="upgrade-name">{{ upgrade.name }}</div>
+            <div class="upgrade-description">{{ upgrade.description }}</div>
+            <div class="upgrade-effect">{{ upgrade.effect }}</div>
+          </div>
+          <div class="upgrade-purchase">
+            <span class="upgrade-price">${{ upgrade.price.toLocaleString() }}</span>
+            <button 
+              class="btn" 
+              @click="buyUpgrade(upgrade)" 
+              :disabled="playerCash < upgrade.price || upgrade.owned"
+            >
+              {{ upgrade.owned ? 'Owned' : 'Buy' }}
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  `
+};
