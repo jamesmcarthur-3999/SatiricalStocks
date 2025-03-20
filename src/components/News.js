e062c1557deb0a23ec79f65b5c1744834ba242aa
jamesmcarthur-3999 <jamesmcarthur3999@gmail.com>
2025-03-20 13:38:33 -0700

    Add News component

---
+++ src/components/News.js
@@ -0,0 +1,17 @@
+// News component
+const News = {
+  props: {
+    newsItems: Array
+  },
+  template: `
+    <div class="news-section">
+      <h2 class="news-title">Breaking News</h2>
+      <ul class="news-list">
+        <li v-for="(item, index) in newsItems" :key="index" class="news-item">
+          <div class="news-headline">{{ item.headline }}</div>
+          <div class="news-effect">Effect: {{ item.effect }}</div>
+        </li>
+      </ul>
+    </div>
+  `
+};
