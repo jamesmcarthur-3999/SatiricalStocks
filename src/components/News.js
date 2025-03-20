d7cc0e3daf2b0d16d0e8e68124c3107704bf4f7a
jamesmcarthur-3999 <jamesmcarthur3999@gmail.com>
2025-03-20 14:07:22 -0700

    Fix News component to use string concatenation

--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,14 @@
 // News component
 var News = {
   props: ['newsItems'],
-  template: `
-    <div class="news-section">
-      <h2 class="news-title">Breaking News</h2>
-      <ul class="news-list">
-        <li v-for="(item, index) in newsItems" :key="index" class="news-item">
-          <div class="news-headline">{{ item.headline }}</div>
-          <div class="news-effect">Effect: {{ item.effect }}</div>
-        </li>
-      </ul>
-    </div>
-  `
+  template: 
+    '<div class="news-section">' +
+      '<h2 class="news-title">Breaking News</h2>' +
+      '<ul class="news-list">' +
+        '<li v-for="(item, index) in newsItems" :key="index" class="news-item">' +
+          '<div class="news-headline">{{ item.headline }}</div>' +
+          '<div class="news-effect">Effect: {{ item.effect }}</div>' +
+        '</li>' +
+      '</ul>' +
+    '</div>'
 };
