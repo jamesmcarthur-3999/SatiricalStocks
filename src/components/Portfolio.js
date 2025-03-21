02fc81675482b723bf3a692cbc7563dedde33bb6
jamesmcarthur-3999 <jamesmcarthur3999@gmail.com>
2025-03-20 18:46:06 -0700

    Update Portfolio component to include quantity selector

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,37 @@
 // Portfolio component
 var Portfolio = {
   props: ['player', 'stocks', 'netWorth'],
+  data: function() {
+    return {
+      selectedQuantities: {},
+      error: null
+    };
+  },
   methods: {
+    getStockQuantity: function(stockId) {
+      return this.selectedQuantities[stockId] || 1;
+    },
+    updateQuantity: function(stockId, quantity) {
+      // Use Vue.set for reactivity
+      Vue.set(this.selectedQuantities, stockId, quantity);
+      this.error = null;
+    },
+    setQuantityError: function(error) {
+      this.error = error;
+    },
     buyStock: function(stock) {
-      this.$emit('buy-stock', stock);
+      const quantity = this.getStockQuantity(stock.id);
+      this.$emit('buy-stock', stock, quantity);
     },
     sellStock: function(stock) {
-      this.$emit('sell-stock', stock);
+      const quantity = this.getStockQuantity(stock.id);
+      // Make sure we don't try to sell more than we own
+      const sellQuantity = Math.min(quantity, stock.owned);
+      this.$emit('sell-stock', stock, sellQuantity);
+    },
+    getMaxBuyQuantity: function(stock) {
+      // Calculate how many shares we can afford
+      return Math.floor(this.player.cash / stock.price);
     }
   },
   template: 
@@ -14,6 +39,7 @@ var Portfolio = {
       '<h2>Portfolio</h2>' +
       '<div class="balance">Cash: ${{ player.cash.toLocaleString() }}</div>' +
       '<div class="net-worth">Net Worth: ${{ netWorth.toLocaleString() }}</div>' +
+      '<div v-if="error" class="error-message">{{ error }}</div>' +
       
       '<h3>Your Stocks:</h3>' +
       '<ul class="stock-list">' +
@@ -26,9 +52,26 @@ var Portfolio = {
             '</span>' +
           '</div>' +
           '<div class="stock-owned" v-if="stock.owned > 0">Owned: {{ stock.owned }}</div>' +
+          
+          '<trade-quantity-selector ' +
+            ':value="getStockQuantity(stock.id)" ' +
+            ':max-quantity="stock.owned > 0 ? stock.owned : getMaxBuyQuantity(stock)" ' +
+            '@update="updateQuantity(stock.id, $event)" ' +
+            '@error="setQuantityError($event)"' +
+          '></trade-quantity-selector>' +
+          
           '<div class="stock-actions">' +
-            '<button class="btn btn-buy" @click="buyStock(stock)" :disabled="player.cash < stock.price">Buy</button>' +
-            '<button class="btn btn-sell" @click="sellStock(stock)" :disabled="stock.owned <= 0">Sell</button>' +
+            '<button class="btn btn-buy" @click="buyStock(stock)" ' +
+              ':disabled="player.cash < (stock.price * getStockQuantity(stock.id))">' +
+              'Buy {{ getStockQuantity(stock.id) > 1 ? getStockQuantity(stock.id) : "" }}' +
+            '</button>' +
+            '<button class="btn btn-sell" @click="sellStock(stock)" ' +
+              ':disabled="stock.owned <= 0">' +
+              'Sell {{ getStockQuantity(stock.id) > 1 ? getStockQuantity(stock.id) : "" }}' +
+            '</button>' +
+          '</div>' +
+          '<div class="transaction-preview" v-if="getStockQuantity(stock.id) > 1">' +
+            '<div>Cost: ${{ (stock.price * getStockQuantity(stock.id)).toLocaleString() }}</div>' +
           '</div>' +
         '</li>' +
       '</ul>' +
